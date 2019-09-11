3c0e9897e2f4ddab5254e74f5b6abc23bcfe847a
Hsin-Hung Chen <cxh@ms1.url.com.tw>
2019-09-12 00:57:47 +0800

    update File

--- 20190910_UIPgaeController_Tutorial/ViewController.swift
+++ 20190910_UIPgaeController_Tutorial/ViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 
 class ViewController: UIViewController,UIScrollViewDelegate {
 
-
     @IBOutlet weak var pageController: UIPageControl!
-    @IBOutlet weak var scrollerView: UIScrollView!
+    @IBOutlet weak var scrw_scrollView: UIScrollView!
     
     var images : [String]  = ["a1","a2","a3","a4"]
-    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
-    
+//    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+
+    var frame = CGRect.zero
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -24,18 +25,19 @@ class ViewController: UIViewController,UIScrollViewDelegate {
         pageController.numberOfPages = images.count
         
         for idx  in  0..<images.count {
-            frame.origin.x = scrollerView.frame.width * CGFloat(idx)
-            frame.size = scrollerView.frame.size
+            frame.origin.x = scrw_scrollView.frame.width * CGFloat(idx)  //  取得每一頁的 X點
+            frame.size = scrw_scrollView.frame.size     //設定大小
             
-            let imgView = UIImageView(frame: frame)
+            let imgView = UIImageView(frame:  frame)   // 產生指定大小的框
+            
             imgView.image = UIImage(named: images[idx])
             
-            self.scrollerView.addSubview(imgView)
+            self.scrw_scrollView.addSubview(imgView)
         }
         
-        scrollerView.contentSize = CGSize(width: scrollerView.frame.width * CGFloat(images.count), height: scrollerView.frame.height)
+        scrw_scrollView.contentSize = CGSize(width: scrw_scrollView.frame.width * CGFloat(images.count), height: scrw_scrollView.frame.height)
         
-        scrollerView.delegate = self
+        scrw_scrollView.delegate = self
        
         pageController.addTarget(self, action: #selector(pageChanged(_:)), for: UIControl.Event.valueChanged)
         
@@ -44,11 +46,12 @@ class ViewController: UIViewController,UIScrollViewDelegate {
     //点击页控件时事件处理
     @objc func pageChanged(_ sender:UIPageControl) {
         //根据点击的页数，计算scrollView需要显示的偏移量
-        var frame = scrollerView.frame
-        frame.origin.x = frame.size.width * CGFloat(sender.currentPage)
+        var frame = scrw_scrollView.frame      //取得 scrollview Frame
+        frame.origin.x = frame.size.width * CGFloat(sender.currentPage)  //計算最近新的 X 軸
         frame.origin.y = 0
-        //展现当前页面内容
-        scrollerView.scrollRectToVisible(frame, animated:true)
+       
+        scrw_scrollView.scrollRectToVisible(frame, animated:true) //展现当前页面内容
+        
     }
     
     
@@ -58,13 +61,12 @@ class ViewController: UIViewController,UIScrollViewDelegate {
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
-        
         pageController.currentPage = Int(pageNumber)
-        
+       
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
+
     }
 
 }
